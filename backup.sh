9d9b6149460aba27473e3e77e6252e203367d76d
koba <10koba01@gmail.com>
2011-04-13 12:39:48 +0900

    fix backup.sh logic

--- backup.sh
+++ backup.sh
@@ -6,18 +6,22 @@ then
   exit 1;
 fi
 
-target=(
-  .aliases
-  .env_vars
-  .zshenv
-  .zsh/.zshrc
-  .emacs
-  .emacs.d/site-lisp/
-  .emacs.d/auto-install/
-  .xmodmap
-  .gitconfig
-)
+src_dir=$HOME
+dst_dir=./HOME
 
-for v in ${target[@]} ; do
-  cp -rv $HOME/$v ./HOME/$v
-done
+rm -rf $dst_dir
+mkdir -p $dst_dir
+mkdir -p $dst_dir/.zsh
+mkdir -p $dst_dir/.emacs.d/site-lisp
+mkdir -p $dst_dir/.emacs.d/auto-install
+
+cmd='cp -fv'
+$cmd $src_dir/.aliases ./$dst_dir/
+$cmd $src_dir/.env_vars ./$dst_dir/
+$cmd $src_dir/.zshenv ./$dst_dir/
+$cmd $src_dir/.zsh/.zshrc ./$dst_dir/.zsh/
+$cmd $src_dir/.emacs ./$dst_dir/
+$cmd $src_dir/.emacs.d/site-lisp/*.el ./$dst_dir/.emacs.d/site-lisp/
+$cmd $src_dir/.emacs.d/auto-install/*.el ./$dst_dir/.emacs.d/auto-install
+$cmd $src_dir/.xmodmap ./$dst_dir/
+$cmd $src_dir/.gitconfig ./$dst_dir/
