4bd2ef79f58a4d4323965c2f7840204292984b01
KOBA Masafumi <10koba01@gmail.com>
2012-08-02 12:28:36 +0900

    setup bin directory

--- setup.sh
+++ setup.sh
@@ -6,9 +6,18 @@ cd $HOME
 CURRDIR=`pwd`
 
 for target in $SRCDIR/.* ; do
-  base=`basename $target`
-  case "$base" in
-    "." | ".." ) continue ;;
-  esac
-  [ ! -L $CURRDIR/$base ] && ln -siv $target
+    base=`basename $target`
+    case "$base" in
+        "." | ".." ) continue ;;
+    esac
+    [ ! -L $CURRDIR/$base ] && ln -siv $target
+done
+
+mkdir -pv $HOME/bin
+cd $HOME/bin
+CURRDIR=`pwd`
+
+for target in $SRCDIR/bin/* ; do
+    base=`basename $target`
+    [ ! -L $CURRDIR/$base ] && ln -siv $target
 done
