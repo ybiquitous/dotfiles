e968ea055505fee606367526553e3eba7f3c7c78
KOBA Masafumi <10koba01@gmail.com>
2012-08-07 13:59:08 +0900

    add config j2s-mode

--- HOME/.emacs.el
+++ HOME/.emacs.el
@@ -62,6 +62,8 @@
 
 ;; javascript
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(setq js2-basic-offset 2)
 
 ;; perl
 (defalias 'perl-mode 'cperl-mode)
