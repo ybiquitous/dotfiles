fb4aa4056936d846ffa0312a013225b2634f0b20
KOBA Masafumi <10koba01@gmail.com>
2012-08-30 15:32:25 +0900

    add byte compile elisp process

--- setup.sh
+++ setup.sh
@@ -21,3 +21,8 @@ for target in $SRCDIR/bin/* ; do
     base=`basename $target`
     [ ! -L $CURRDIR/$base ] && ln -siv $target
 done
+
+# byte compile emacs-lisp
+cd $SRCDIR/.emacs.d/
+emacs -L auto-install/ -batch -f batch-byte-compile init.el
+emacs -L auto-install/ -batch -f batch-byte-compile auto-install/*.el
