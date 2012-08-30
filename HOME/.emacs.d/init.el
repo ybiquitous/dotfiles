06c97b3ea1f25c57deeb0adf741cccd302eeed51
KOBA Masafumi <10koba01@gmail.com>
2012-08-30 15:57:03 +0900

    update js2-mode

--- HOME/.emacs.d/init.el
+++ HOME/.emacs.d/init.el
@@ -71,6 +71,7 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 
 ;; javascript
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (setq js2-basic-offset 2)
