fb3083c0265c487c14021e71882271d182fe8f8e
KOBA Masafumi <10koba01@gmail.com>
2012-09-28 09:54:31 +0900

    blink-cursor off.

--- HOME/.emacs.d/init.el
+++ HOME/.emacs.d/init.el
@@ -4,6 +4,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
+ '(blink-cursor nil)
  '(column-number-mode t)
  '(global-font-lock-mode t)
  '(indent-tabs-mode nil)
