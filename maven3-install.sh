78b5098acc2f1af32e65f0315a362db22c6fe5bc
KOBA Masafumi <10koba01@gmail.com>
2013-02-19 18:58:14 +0900

    Fixed maven3-install.sh

--- maven3-install.sh
+++ maven3-install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-VERSION=3.0.4
-DISTFILE=apache-maven-${VERSION}-bin.tar.gz
-DISTDIR=apache-maven-${VERSION}
+[ -z "$1" ] && echo "usage: $0 <version>" && exit 1
+
+VERSION="$1"
+NAME=apache-maven-$VERSION
+FILE=$NAME-bin.tar.gz
+URL=http://www.apache.org/dist/maven/maven-3/$VERSION/binaries
 
 #---------
 cd /opt
-mkdir maven3
+mkdir -p maven3
 if [ $? -ne 0 ] ; then
   exit 1
 fi
@@ -14,19 +17,17 @@ cd maven3
 #---------
 
 #---------
-curl -O http://ftp.meisei-u.ac.jp/mirror/apache/dist/maven/binaries/$DISTFILE
-curl -O http://www.apache.org/dist/maven/binaries/$DISTFILE.md5
-echo "  $DISTFILE" >> $DISTFILE.md5
-md5sum -c $DISTFILE.md5
+curl -O $URL/$FILE
+echo "`curl -s $URL/$FILE.md5` *$FILE" | md5sum -c
 if [ $? -ne 0 ] ; then
   echo "download error"
   exit 1;
 fi
-rm -f $DISTFILE.md5
-tar xfz $DISTFILE
+tar xfz $FILE
+rm -f $FILE
 #---------
 
 #---------
-ln -s $DISTDIR latest
+ln -s $NAME latest
 latest/bin/mvn -version
 #---------
