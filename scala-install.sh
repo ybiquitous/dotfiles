c16206974804df415a2cbd7d63ce4248de1a6c59
KOBA Masafumi <10koba01@gmail.com>
2013-06-27 10:10:16 +0900

    Update scala-install.sh

--- scala-install.sh
+++ scala-install.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-[ -z "$1" ] && echo "usage: $0 <version>" && exit 1
+if [ $# -ne 2 ]; then
+    echo "usage: $0 <version> <install_dir>"
+    exit 1
+fi
 
 VERSION="$1"
+INSTALL_DIR="$2"
 NAME=scala-$VERSION
 FILE=$NAME.tgz
 URL=http://www.scala-lang.org/downloads/distrib/files
 
 . `readlink -e lib.sh`
 
-make_install_dir /opt/scala
+make_install_dir $INSTALL_DIR
 download $URL $FILE
 tar xfz $FILE
 rm -f $FILE
