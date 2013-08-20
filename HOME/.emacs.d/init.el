89daa30a6f016de056816eb062cd68025f023dc7
KOBA Masafumi <10koba01@gmail.com>
2013-08-20 10:24:06 +0900

    Edit init.el

--- HOME/.emacs.d/init.el
+++ HOME/.emacs.d/init.el
@@ -1,11 +1,12 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
  '(blink-cursor-mode nil)
  '(column-number-mode t)
+ '(electric-pair-mode t)
  '(global-font-lock-mode t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
@@ -14,7 +15,7 @@
  '(make-backup-files nil)
  '(read-buffer-completion-ignore-case t)
  '(read-file-name-completion-ignore-case t)
- '(scroll-bar-mode 'right)
+ '(scroll-bar-mode (quote right))
  '(show-paren-mode t)
  '(show-trailing-whitespace t)
  '(size-indication-mode nil)
@@ -23,10 +24,10 @@
  '(transient-mark-mode t)
  '(x-select-enable-clipboard t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:background "black" :foreground "white" :height 110)))))
 
 (eval-after-load "dired-aux"
