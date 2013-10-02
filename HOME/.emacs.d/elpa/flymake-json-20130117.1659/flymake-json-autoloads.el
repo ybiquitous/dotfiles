88e0419de558386a58509965157cfa3327cfa3db
KOBA Masafumi <10koba01@gmail.com>
2013-10-02 12:01:00 +0900

    Update all elisps

--- HOME/.emacs.d/elpa/flymake-json-20130117.1659/flymake-json-autoloads.el
+++
@@ -1,36 +0,0 @@
-;;; flymake-json-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (flymake-json-maybe-load flymake-json-load) "flymake-json"
-;;;;;;  "../../../../../.emacs.d/elpa/flymake-json-20130117.1659/flymake-json.el"
-;;;;;;  "e3d2422f82aca6a832a42c2446d2e259")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/flymake-json-20130117.1659/flymake-json.el
-
-(autoload 'flymake-json-load "flymake-json" "\
-Configure flymake mode to check the current buffer's javascript syntax.
-
-\(fn)" t nil)
-
-(autoload 'flymake-json-maybe-load "flymake-json" "\
-Call `flymake-json-load' if this file appears to be json.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/flymake-json-20130117.1659/flymake-json-pkg.el"
-;;;;;;  "../../../../../.emacs.d/elpa/flymake-json-20130117.1659/flymake-json.el")
-;;;;;;  (20746 7942 456632))
-
-;;;***
-
-(provide 'flymake-json-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymake-json-autoloads.el ends here
