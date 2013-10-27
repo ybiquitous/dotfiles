61d123e12c7a35ea9b149487838184b5546a25d0
KOBA Masafumi <10koba01@gmail.com>
2013-10-27 13:11:39 +0900

    Removed dropdown-list (use helm)

--- HOME/.emacs.d/init.el
+++ HOME/.emacs.d/init.el
@@ -91,10 +91,20 @@
 
 ;; yasnippet
 (yas-global-mode t)
-(require 'dropdown-list)
-(setq yas-prompt-functions '(yas-dropdown-prompt
-                             yas-ido-prompt
-                             yas-completing-prompt))
+(defun my-yas-prompt (prompt choices &optional display-fn)
+  (let* ((names (loop for choice in choices
+                      collect (or (and display-fn (funcall display-fn choice))
+                                  coice)))
+         (selected (helm-other-buffer
+                    `(((name . ,(format "%s" prompt))
+                       (candidates . names)
+                       (action . (("Insert snippet" . (lambda (arg) arg))))))
+                    "*helm yas/prompt*")))
+    (if selected
+        (let ((n (position selected names :test 'equal)))
+          (nth n choices))
+      (signal 'quit "user quit!"))))
+(custom-set-variables '(yas-prompt-functions '(my-yas-prompt)))
 
 ;; scala
 (require 'scala-mode2)
