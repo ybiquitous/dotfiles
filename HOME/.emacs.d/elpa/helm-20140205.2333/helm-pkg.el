48f6f3e6a4e14eb143c7bfbf73f69390e90d8e40
ybiquitous <10koba01@gmail.com>
2014-02-20 09:50:40 +0900

    Elpa Upgrade

--- HOME/.emacs.d/elpa/helm-20140205.2333/helm-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm" "20140205.2333" "Helm is an Emacs incremental and narrowing framework" 'nil)
---
+++ HOME/.emacs.d/elpa/web-mode-20140219.1618/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20140219.1618" "major mode for editing html templates" 'nil)
