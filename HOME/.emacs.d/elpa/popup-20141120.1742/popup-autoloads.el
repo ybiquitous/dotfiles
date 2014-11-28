818f8febb7ef4243de596d2ef58bc7c9bf56c069
ybiquitous <10koba01@gmail.com>
2014-11-28 14:25:03 +0900

    Updated ELPA

---
+++ HOME/.emacs.d/elpa/popup-20141120.1742/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21624 1584
+;;;;;;  421313 52000))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
