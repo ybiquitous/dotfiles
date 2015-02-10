191762460b7330bbf19aa6969ecf9f52ad97fce1
ybiquitous <10koba01@gmail.com>
2015-02-10 12:57:47 +0900

    Modify Emacs default face

--- HOME/.emacs.d/init.el
+++ HOME/.emacs.d/init.el
@@ -47,7 +47,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "black" :foreground "white" :height 110)))))
+ '(default ((t (:background "black" :foreground "white")))))
 
 (load-file "~/.emacs.d/init-env.elc")
 
