949690607d91fdcbf4ea003fcc09ef50e967370c
ybiquitous <10koba01@gmail.com>
2015-03-05 00:07:52 +0900

    Make compile java hook local

--- HOME/.emacs.d/inits/init-java.el
+++ HOME/.emacs.d/inits/init-java.el
@@ -1,7 +1,7 @@
 (defun java-compile ()
-  (shell-command (format "javac %s" buffer-file-name)))
+  (shell-command (format "javac -Xlint %s" buffer-file-name)))
 (add-hook 'java-mode-hook
-          (lambda () (add-hook 'after-save-hook 'java-compile)))
+          (lambda () (add-hook 'after-save-hook 'java-compile nil 'make-it-local)))
 
 (provide 'init-java)
 ;;; init-java.el ends here
