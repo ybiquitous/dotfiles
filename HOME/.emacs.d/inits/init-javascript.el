96317281af6374c2ad7b780f93916198c6ef9f46
ybiquitous <10koba01@gmail.com>
2015-04-10 22:48:58 +0900

    JSCS formatting support

--- HOME/.emacs.d/inits/init-javascript.el
+++ HOME/.emacs.d/inits/init-javascript.el
@@ -1,25 +1,32 @@
 ;; flycheck-jscs
-(flycheck-def-config-file-var flycheck-jscsrc javascript-jscs ".jscsrc"
-  :safe #'stringp)
+;; (flycheck-def-config-file-var flycheck-jscsrc javascript-jscs ".jscsrc"
+;;   :safe #'stringp)
 
-(flycheck-define-checker javascript-jscs
-  "A JavaScript style checker using JSCS.
+;; (flycheck-define-checker javascript-jscs
+;;   "A JavaScript style checker using JSCS.
 
-See URL `http://jscs.info/'."
-  :command ("jscs"
-            "--reporter" "checkstyle"
-            (config-file "--config" flycheck-jscsrc)
-            source)
-  :error-parser flycheck-parse-checkstyle
-  :modes (js-mode js2-mode js3-mode)
-  :next-checkers (javascript-jshint))
-(add-to-list 'flycheck-checkers 'javascript-jscs)
+;; See URL `http://jscs.info/'."
+;;   :command ("jscs"
+;;             "--reporter" "checkstyle"
+;;             (config-file "--config" flycheck-jscsrc)
+;;             source)
+;;   :error-parser flycheck-parse-checkstyle
+;;   :modes (js-mode js2-mode js3-mode)
+;;   :next-checkers (javascript-jshint))
+;; (add-to-list 'flycheck-checkers 'javascript-jscs)
 
-;; formatting by JSCS
+(defcustom jscs-format-enabled t
+  "*Turn on/off JSCS formatting"
+  :type 'boolean)
+
 (defun jscs-format ()
   (interactive)
-  (shell-command (concat "jscs --fix " buffer-file-name))
-  (revert-buffer t t)
+  (if jscs-format-enabled
+      (progn
+        (shell-command (concat "jscs --fix " buffer-file-name) "*JSCS*")
+        (revert-buffer t t)
+        )
+    )
   )
 
 ;; hook
@@ -27,9 +34,8 @@ See URL `http://jscs.info/'."
           (lambda()
             (electric-indent-mode t)
             (c-set-offset 'case-label '+)
-            (local-set-key (kbd "C-c C-b") 'web-beautify-js)
             (local-set-key (kbd "C-c C-d") 'js-doc-insert-function-doc)
-            (add-hook 'after-save-hook 'jscs-format)
+            (add-hook 'after-save-hook 'jscs-format t t)
             ))
 
 (provide 'init-javascript)
