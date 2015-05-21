3c84cd93ccdfa28c011c873044e72003a3d4b4ea
ybiquitous <10koba01@gmail.com>
2015-05-21 09:47:52 +0900

    Emacs: remove no error JSCS output buffer

--- HOME/.emacs.d/inits/init-javascript.el
+++ HOME/.emacs.d/inits/init-javascript.el
@@ -33,6 +33,8 @@ See URL `http://jscs.info/'."
 (defun jscs-format-disable () (interactive)
   (custom-set-variables '(jscs-format-enabled nil)))
 
+(defvar jscs-buffer-name "*JSCS*")
+
 (defun jscs-format ()
   (if jscs-format-enabled
       (progn
@@ -40,8 +42,15 @@ See URL `http://jscs.info/'."
          (format "jscs --fix --reporter inline %s %s"
                  (if jscs-preset (concat "--preset " jscs-preset) "")
                  buffer-file-name)
-         "*JSCS*")
+         jscs-buffer-name)
         (revert-buffer t t)
+
+        ;; if no errors, close window and buffer
+        (if (eq (buffer-size (get-buffer jscs-buffer-name)) 0)
+            (progn
+              (if (not (one-window-p))
+                  (delete-window (get-buffer-window jscs-buffer-name)))
+              (kill-buffer jscs-buffer-name)))
         )))
 
 (add-hook 'js-mode-hook
