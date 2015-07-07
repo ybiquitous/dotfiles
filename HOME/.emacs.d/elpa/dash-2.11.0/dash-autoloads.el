409e4212c82a79bca5fd2bfb8147ad7198123b1f
ybiquitous <10koba01@gmail.com>
2015-07-07 09:56:24 +0900

    Update elisp

---
+++ HOME/.emacs.d/elpa/dash-2.11.0/dash-autoloads.el
@@ -0,0 +1,17 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/dash-2.11.0/dash-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/dash-2.11.0/dash.el") (21915
+;;;;;;  8982 530273 500000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ HOME/.emacs.d/elpa/with-editor-2.1.0/with-editor-autoloads.el
@@ -0,0 +1,18 @@
+;;; with-editor-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/with-editor-2.1.0/with-editor-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/with-editor-2.1.0/with-editor-pkg.el"
+;;;;;;  "../../../../../.emacs.d/elpa/with-editor-2.1.0/with-editor.el")
+;;;;;;  (21915 8970 807603 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; with-editor-autoloads.el ends here
