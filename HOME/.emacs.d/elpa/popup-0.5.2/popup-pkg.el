ab60672357e722a1fbdc22755df90a0b59a4964b
ybiquitous <10koba01@gmail.com>
2015-08-10 10:14:42 +0900

    Update ELPA

--- HOME/.emacs.d/elpa/popup-0.5.2/popup-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "popup" "0.5.2" "Visual Popup User Interface" (quote ((cl-lib "0.3"))))
---
+++ HOME/.emacs.d/elpa/popup-0.5.3/popup-autoloads.el
@@ -0,0 +1,17 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/popup-0.5.3/popup-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/popup-0.5.3/popup.el") (21959
+;;;;;;  64134 256449 300000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; popup-autoloads.el ends here
