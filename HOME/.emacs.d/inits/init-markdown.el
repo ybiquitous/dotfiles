78ded767cbccccd43040c449eedda6debd27d5c4
ybiquitous <10koba01@gmail.com>
2015-08-15 12:33:20 +0900

    change 'markdown-mode' configuration

--- HOME/.emacs.d/inits/init-markdown.el
+++ HOME/.emacs.d/inits/init-markdown.el
@@ -1,11 +1,13 @@
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 
 (add-hook 'markdown-mode-hook
           (lambda()
             (custom-set-variables
-             '(markdown-command "marked --gfm")
+             '(markdown-command "marked")
+             '(markdown-content-type "application/xhtml+xml")
              '(markdown-css-path "http://code.ngoldman.me/gfm.css/gfm.css")
+             '(markdown-xhtml-header-content "<script>window.onload = function() { document.body.className = 'markdown-body' }</script>")
              )
             ))
 
