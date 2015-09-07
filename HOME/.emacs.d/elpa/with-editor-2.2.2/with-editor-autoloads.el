358369d215356099d1c74be291c4e6717fd83317
ybiquitous <10koba01@gmail.com>
2015-09-07 10:31:06 +0900

    Update `magit`

---
+++ HOME/.emacs.d/elpa/with-editor-2.2.2/with-editor-autoloads.el
@@ -0,0 +1,18 @@
+;;; with-editor-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/with-editor-2.2.2/with-editor-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/with-editor-2.2.2/with-editor-pkg.el"
+;;;;;;  "../../../../../.emacs.d/elpa/with-editor-2.2.2/with-editor.el")
+;;;;;;  (21996 59260 314589 900000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; with-editor-autoloads.el ends here
