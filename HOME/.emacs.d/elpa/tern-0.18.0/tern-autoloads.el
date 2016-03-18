98a4b34989a3c92dab527a51a02d61f7b546cb26
ybiquitous <10koba01@gmail.com>
2016-03-18 20:05:45 +0900

    update tern-mode to 0.18.0

--- HOME/.emacs.d/elpa/tern-0.18.0/tern-autoloads.el
+++ HOME/.emacs.d/elpa/tern-0.18.0/tern-autoloads.el
@@ -1,10 +1,9 @@
 ;;; tern-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (tern-mode tern-use-server) "tern" "tern.el" (22155
-;;;;;;  26993 648872 811000))
+;;;### (autoloads nil "tern" "tern.el" (22251 57496 358214 223000))
 ;;; Generated autoloads from tern.el
 
 (autoload 'tern-use-server "tern" "\
@@ -19,15 +18,9 @@ Minor mode binding to the Tern JavaScript analyzer
 
 ;;;***
 
-;;;### (autoloads nil nil ("tern-pkg.el") (22155 26993 675251 477000))
-
-;;;***
-
-(provide 'tern-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; tern-autoloads.el ends here
--- HOME/.emacs.d/elpa/tern-0.18.0/tern.el
+++ HOME/.emacs.d/elpa/tern-0.18.0/tern.el
@@ -2,7 +2,7 @@
 
 ;; Author: Marijn Haverbeke
 ;; URL: http://ternjs.net/
-;; Package-Version: 0.17.0
+;; Package-Version: 0.18.0
 ;; Version: 0.0.1
 ;; Package-Requires: ((json "1.2") (cl-lib "0.5") (emacs "24"))
 
