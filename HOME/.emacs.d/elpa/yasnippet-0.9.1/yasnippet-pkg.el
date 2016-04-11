f9fb890bbc96d529cb096041bc6fb8f00726d2fc
ybiquitous <10koba01@gmail.com>
2016-04-12 00:35:16 +0900

    update yasnippet

---
+++ HOME/.emacs.d/elpa/yasnippet-0.9.1/yasnippet-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from yasnippet.el
+(define-package "yasnippet" "0.9.1" "Yet another snippet extension for Emacs." 'nil :url "http://github.com/capitaomorte/yasnippet" :keywords '("convenience" "emulation"))
