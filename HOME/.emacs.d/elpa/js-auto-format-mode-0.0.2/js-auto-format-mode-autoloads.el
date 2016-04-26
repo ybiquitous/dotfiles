14dc955da9215a69d9c432932c6a8694a6c33a76
ybiquitous <10koba01@gmail.com>
2016-04-26 09:54:24 +0900

    Update js-auto-format-mode to 0.0.2

---
+++ HOME/.emacs.d/elpa/js-auto-format-mode-0.0.2/js-auto-format-mode-autoloads.el
@@ -0,0 +1,47 @@
+;;; js-auto-format-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "js-auto-format-mode" "js-auto-format-mode.el"
+;;;;;;  (22302 48052 798689 870000))
+;;; Generated autoloads from js-auto-format-mode.el
+
+(defvar js-auto-format-command "eslint" "\
+JavaScript auto format command.")
+
+(custom-autoload 'js-auto-format-command "js-auto-format-mode" t)
+
+(defvar js-auto-format-command-args "--fix" "\
+JavaScript auto format command arguments.")
+
+(custom-autoload 'js-auto-format-command-args "js-auto-format-mode" t)
+
+(defvar js-auto-format-enabled t "\
+JavaScript auto format enabled.")
+
+(custom-autoload 'js-auto-format-enabled "js-auto-format-mode" t)
+
+(autoload 'js-auto-format-execute "js-auto-format-mode" "\
+Format JavaScript source code.
+
+\(fn)" nil nil)
+
+(autoload 'js-auto-format-mode "js-auto-format-mode" "\
+Minor mode for auto-formatting JavaScript code
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("js-auto-format-mode-pkg.el") (22302 48052
+;;;;;;  979152 360000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; js-auto-format-mode-autoloads.el ends here
