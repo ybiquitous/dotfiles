a987a28c8e158201d7d6206ca3f4177dde56f1a9
ybiquitous <10koba01@gmail.com>
2016-06-03 11:06:06 +0900

    Installed robe-mode (Emacs)

--- HOME/.emacs.d/inits/init-ruby.el
+++ HOME/.emacs.d/inits/init-ruby.el
@@ -1,7 +1,11 @@
+(require 'company-robe)
+
 (add-hook 'ruby-mode-hook
           (lambda ()
             (custom-set-variables
              '(ruby-insert-encoding-magic-comment nil))
+            (robe-mode t)
+            (add-to-list 'company-backends 'company-robe)
             ))
 
 (provide 'init-ruby)
