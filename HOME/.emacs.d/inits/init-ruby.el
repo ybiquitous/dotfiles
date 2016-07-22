74114fe4d68dbd9eacad06179eae81c795949be0
ybiquitous <10koba01@gmail.com>
2016-07-22 17:04:08 +0900

    Add Emacs function `rails-jump-to-test`

--- HOME/.emacs.d/inits/init-ruby.el
+++ HOME/.emacs.d/inits/init-ruby.el
@@ -8,4 +8,17 @@
             (add-to-list 'company-backends 'company-robe)
             ))
 
+(defun rails-jump-to-test ()
+  "Rails Jump to Test"
+  (interactive)
+  (let* ((test-file-name (replace-regexp-in-string ".rb" "_test.rb" buffer-file-name))
+         (test-file-name (replace-regexp-in-string "/app/" "/test/" test-file-name)))
+    (unless (file-exists-p test-file-name)
+      (progn (setq test-file-name (replace-regexp-in-string "_test.rb" "_spec.rb" test-file-name)))
+      (progn (setq test-file-name (replace-regexp-in-string "/test/" "/spec/" test-file-name)))
+      )
+    (if (file-exists-p test-file-name)
+        (find-file test-file-name)
+      (message "No test file: %s" test-file-name))))
+
 (provide 'init-ruby)
