cee6784a62de35fcadd215edd706735dc4b5e9b3
ybiquitous <10koba01@gmail.com>
2016-09-12 00:36:37 +0900

    Use stylelint for linting CSS

--- HOME/.emacs.d/inits/init-css.el
+++ HOME/.emacs.d/inits/init-css.el
@@ -1,11 +1,11 @@
 (require 'flycheck)
 
 ;;; stylelint (https://github.com/flycheck/flycheck/issues/785)
-(flycheck-define-checker stylelint-scss-lint
+(flycheck-define-checker stylelint-lint
   "A SCSS checker using stylelint.
 
 See URL `http://stylelint.io/'."
-  :command ("stylelint" "--syntax" "scss" source)
+  :command ("stylelint" source)
   :error-patterns
   ((error line-start
           (zero-or-more (any space)) line ":" column
@@ -13,15 +13,16 @@ See URL `http://stylelint.io/'."
           (message)
           (zero-or-more (any space))
           line-end))
-  :modes (scss-mode))
+  :modes (css-mode scss-mode))
 ;;; stylelint
+
+(add-to-list 'flycheck-checkers 'stylelint-lint)
 
 (add-hook 'scss-mode-hook
           (lambda()
             (custom-set-variables
              '(scss-sass-command "node-sass")
              '(scss-compile-at-save nil))
-            (add-to-list 'flycheck-checkers 'stylelint-scss-lint)
             ))
 
 (provide 'init-css)
