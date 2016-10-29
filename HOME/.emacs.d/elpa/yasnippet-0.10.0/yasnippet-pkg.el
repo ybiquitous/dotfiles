0c410631e1271904d478183f08ea70f1ec20c645
ybiquitous <10koba01@gmail.com>
2016-10-29 18:31:11 +0900

    Update elpa

--- HOME/.emacs.d/elpa/yasnippet-0.10.0/yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from yasnippet.el
-(define-package "yasnippet" "0.10.0" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :url "http://github.com/capitaomorte/yasnippet" :keywords '("convenience" "emulation"))
