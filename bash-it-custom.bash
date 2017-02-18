a76de98e0c3cc2afe2a18966ecd64265169aea7b
ybiquitous <10koba01@gmail.com>
2017-02-18 17:31:30 +0900

    Auto-enabled .nvmrc in project

--- bash-it-custom.bash
+++ bash-it-custom.bash
@@ -6,3 +6,15 @@ if [ -t 1 ]; then
 fi
 
 alias rm='rm -i'
+
+# Auto-enabled .nvmrc in project
+# https://github.com/creationix/nvm/pull/1057
+function nvm_use_if_needed () {
+  [[ -r ./.nvmrc  && -s ./.nvmrc ]] || return
+  WANTED=$(sed 's/v//' < ./.nvmrc)
+  CURRENT=$(hash node 2>/dev/null && node -v | sed 's/v//')
+  if [ "$WANTED" != "$CURRENT" ]; then
+      nvm use
+  fi
+}
+export PROMPT_COMMAND="$PROMPT_COMMAND ; nvm_use_if_needed"
