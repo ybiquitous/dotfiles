8d371071645f8a69ff6f488e57eaf8cb6dbe8bdd
ybiquitous <10koba01@gmail.com>
2017-02-23 11:38:34 +0900

    Add install brew script

---
+++ install/brew.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eu
+
+# https://brew.sh/index_ja.html
+if [[ -z $(command -v brew) ]]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+brew install \
+     awscli \
+     emacs \
+     heroku \
+     imagemagick@6 \
+     jq \
+     mysql \
+     postgresql \
+     rbenv \
+     redis
