3a5a084a4ca45add9e693291772698b067097a1f
ybiquitous <10koba01@gmail.com>
2017-02-23 11:31:20 +0900

    Auto-enabled `nvm use` on change directory

--- install/bash-it-custom.bash
+++ install/bash-it-custom.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -t 1 ]; then
+if [[ -t 1 ]]; then
   bind '"\C-p":history-search-backward'
   bind '"\C-n":history-search-forward'
 fi
@@ -11,3 +11,12 @@ export EDITOR=emacsclient
 
 export PATH="$HOME/.yarn/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
+
+# https://github.com/creationix/nvm/pull/1057
+function nvm_use_if_needed () {
+  [[ -f package.json ]] || return
+  local WANTED=$(jq -r '.engines.node' package.json)
+  local CURRENT=$(hash node 2>/dev/null && node -v | sed 's/v//')
+  [[ $WANTED != $CURRENT ]] && nvm use "$WANTED"
+}
+export PROMPT_COMMAND="$PROMPT_COMMAND ; nvm_use_if_needed"
