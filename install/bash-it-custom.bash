107692d00610e2b13c99078492b131523171eda5
ybiquitous <10koba01@gmail.com>
2017-02-25 11:32:04 +0900

    Improve bash function `nvm_use_if_needed`

--- install/bash-it-custom.bash
+++ install/bash-it-custom.bash
@@ -14,9 +14,18 @@ export PATH="$HOME/bin:$PATH"
 
 # https://github.com/creationix/nvm/pull/1057
 function nvm_use_if_needed () {
+  [[ -f .nvmrc ]] && nvm use && return
+
   [[ -f package.json ]] || return
+
   local WANTED=$(jq -r '.engines.node' package.json)
+  [[ $WANTED == null ]] && nvm use default && return
+
   local CURRENT=$(hash node 2>/dev/null && node -v | sed 's/v//')
-  [[ $WANTED != $CURRENT ]] && nvm use "$WANTED"
+  if [[ $WANTED != $CURRENT ]]; then
+    nvm use "$WANTED"
+  else
+    nvm use default
+  fi
 }
 export PROMPT_COMMAND="$PROMPT_COMMAND ; nvm_use_if_needed"
