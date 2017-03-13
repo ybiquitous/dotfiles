bb973f5195abc7289eee886ced8e3ce2eb2d7878
ybiquitous <10koba01@gmail.com>
2017-03-13 12:54:10 +0900

    Fix automatic nvm script

--- install/bash-it-custom.bash
+++ install/bash-it-custom.bash
@@ -19,10 +19,11 @@ load-nvmrc() {
   local nvmrc_path="$(nvm_find_nvmrc)"
 
   if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+    local nvmrc_content=$(< "${nvmrc_path}")
+    local nvmrc_node_version=$(nvm version "$nvmrc_content")
 
     if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
+      echo "node version '$nvmrc_content' is not installed, please run: 'nvm install'"
     elif [ "$nvmrc_node_version" != "$node_version" ]; then
       nvm use
     fi
