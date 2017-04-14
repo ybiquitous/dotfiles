98887d7307234c860e43a47c4b36ccfd3cd1b073
ybiquitous <10koba01@gmail.com>
2017-04-14 18:18:20 +0900

    Add message about Yarn reinstallation

--- install/yarn.sh
+++ install/yarn.sh
@@ -2,10 +2,16 @@
 set -eu
 
 # https://yarnpkg.com/en/docs/install#alternatives-tab
+
 if [[ -d ~/.yarn ]]; then
+  echo
   echo 'Yarn is installed already:'
-  echo ''
-  echo $(command -v yarn)
+  echo
+  echo "   $(command -v yarn)"
+  echo
+  echo 'If you want to reinstall, please run following command:'
+  echo
+  echo '   rm -rf ~/.yarn'
 else
   dummy_profile=/tmp/yarn-dummy-profile
   touch $dummy_profile
