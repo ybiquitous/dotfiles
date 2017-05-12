eac256ef00a2dcccc075e46d8623eb719315fc5f
ybiquitous <10koba01@gmail.com>
2017-05-12 11:54:43 +0900

    Update seq.el

--- HOME/.emacs.d/elpa/seq-2.20/seq-autoloads.el
+++ HOME/.emacs.d/elpa/seq-2.20/seq-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("seq-24.el" "seq-25.el" "seq-pkg.el" "seq.el")
-;;;;;;  (22548 27476 106442 519000))
+;;;;;;  (22805 9179 0 0))
 
 ;;;***
 
