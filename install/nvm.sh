a86dc3d1e6f147121d1f3d677d043fb4b8bc4271
ybiquitous <10koba01@gmail.com>
2017-05-26 13:04:40 +0900

    Update nvm version

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -eu
 
-NVM_VERSION=v0.33.0
+NVM_VERSION=v0.33.2
 
 # https://github.com/creationix/nvm#install-script
+
 if [[ ! -d ~/.nvm ]]; then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | PROFILE="/tmp/nvm_dummpy_profile" bash
 fi
