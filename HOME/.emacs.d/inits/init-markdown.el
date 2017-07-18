f83a4147f8ac9c6142a18ed18e374f393d968b29
ybiquitous <10koba01@gmail.com>
2017-07-18 21:36:35 +0900

    update markdown CSS/JS

--- HOME/.emacs.d/inits/init-markdown.el
+++ HOME/.emacs.d/inits/init-markdown.el
@@ -7,12 +7,11 @@
              '(markdown-command "marked")
              '(markdown-content-type "application/xhtml+xml")
              '(markdown-css-paths
-               '("https://sindresorhus.com/github-markdown-css/github-markdown.css"
-                 "https://cdn.jsdelivr.net/highlight.js/9.4.0/styles/default.min.css"))
+               '("https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.8.0/github-markdown.min.css"
+                 "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css"))
              '(markdown-xhtml-header-content "
-<script>document.addEventListener('DOMContentLoaded', function() { document.body.className = 'markdown-body' })</script>
-<script src='https://cdn.jsdelivr.net/highlight.js/9.4.0/highlight.min.js'></script>
-<script>hljs.initHighlightingOnLoad();</script>")
+<script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js'></script>
+<script>document.addEventListener('DOMContentLoaded', function() { document.body.className = 'markdown-body'; hljs.initHighlightingOnLoad(); })</script>")
              )))
 
 
