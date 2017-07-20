06c4e95f43177273d0850dda0d1202dddc91c2c0
ybiquitous <10koba01@gmail.com>
2017-07-20 13:00:48 +0900

    refactor dired config via use-package

--- HOME/.emacs.d/inits/60_dired.el
+++ HOME/.emacs.d/inits/60_dired.el
@@ -1,5 +1,9 @@
-(require 'wdired)
-
-(setq wdired-allow-to-change-permissions t)
-
-(define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
+(use-package dired
+  :bind (
+          :map dired-mode-map
+          ("e" . wdired-change-to-wdired-mode))
+  :init
+  (require 'wdired)
+  :config
+  (setq wdired-allow-to-change-permissions t)
+  )
