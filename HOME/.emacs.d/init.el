b545114028a9d21ce3425f07ff429d71be8411c9
ybiquitous <10koba01@gmail.com>
2017-07-20 12:17:43 +0900

    refactor Emacs config

--- HOME/.emacs.d/init.el
+++ HOME/.emacs.d/init.el
@@ -66,17 +66,6 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; key bindings
-(global-set-key (kbd "C-h")        'delete-backward-char)
-(global-set-key (kbd "M-h")        'backward-kill-word)
-(global-set-key (kbd "C-?")        'help-command)
-(global-set-key (kbd "C-c l")      'toggle-truncate-lines)
-(global-set-key (kbd "C-c C-v")    'browse-url-of-buffer)
-(global-set-key (kbd "C-c <up>")   'text-scale-increase)
-(global-set-key (kbd "C-c <down>") 'text-scale-decrease)
-(global-set-key (kbd "C-c 0")      'text-scale-adjust)
-(global-set-key (kbd "C-c C-r")    'eval-buffer)
-
 ;; package (https://github.com/melpa/melpa)
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
@@ -87,9 +76,6 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
-
-;; exec-path
-(if (not (equal system-type 'windows-nt)) (exec-path-from-shell-initialize))
 
 ;; init-loader
 (require 'init-loader)
--- HOME/.emacs.d/inits/01_common.el
+++ HOME/.emacs.d/inits/01_common.el
@@ -29,3 +29,10 @@
 
 ;; frame title
 (setq frame-title-format '("" "%b @ Emacs " emacs-version))
+
+;; exec-path (https://github.com/jwiegley/use-package#conditional-loading)
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
