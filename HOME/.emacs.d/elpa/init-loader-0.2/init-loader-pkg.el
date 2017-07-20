474613af807dab2613c447c39d9f6acbde837f38
ybiquitous <ybiquitous@gmail.com>
2017-07-20 23:09:02 +0900

    uninstall init-loader.el

--- HOME/.emacs.d/elpa/init-loader-0.2/init-loader-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "init-loader" "0.2" "Loader for configuration files" 'nil :commit "128ee76adbf431f0b8c30a3a29cb20c9c5100cde" :url "https://github.com/emacs-jp/init-loader/")
