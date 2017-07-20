7f47693c06d8f23a38e2c8b3193c6fc6d3120cc5
ybiquitous <ybiquitous@gmail.com>
2017-07-21 00:30:17 +0900

    uninstall highlight-symbol.el

--- HOME/.emacs.d/inits/08_highlight-symbol.el
+++ HOME/.emacs.d/inits/08_highlight-symbol.el
@@ -1,6 +1,7 @@
-(add-hook 'prog-mode-hook 'highlight-symbol-mode)
-(add-hook 'prog-mode-hook 'highlight-symbol-nav-mode) ;; move by M-p and M-n
-
 (use-package highlight-symbol
+  :ensure t
   :bind (:map highlight-symbol-mode
-          ("C-c r" . highlight-symbol-query-replace)))
+          ("C-c r" . highlight-symbol-query-replace))
+  :init
+  (add-hook 'prog-mode-hook 'highlight-symbol-mode)
+  (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)) ;; move by M-p and M-n
