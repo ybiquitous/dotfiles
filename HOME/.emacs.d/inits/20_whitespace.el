23ffe7da9a41cc14354c36f6e79a0e2105daa390
ybiquitous <ybiquitous@gmail.com>
2017-07-21 00:17:50 +0900

    refactor indent

--- HOME/.emacs.d/inits/20_whitespace.el
+++ HOME/.emacs.d/inits/20_whitespace.el
@@ -1,8 +1,9 @@
 (use-package whitespace
   :config
-  (setq whitespace-action '(auto-cleanup)
-        whitespace-display-mappings '((space-mark ?\u3000 [?\u25a1])
-                                      (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t]))
-        whitespace-space-regexp "\\(\\u3000+\\)"
-        whitespace-line-column 100
-        whitespace-style '(face tabs spaces trailing lines-tail space-mark tab-mark)))
+  (setq
+    whitespace-action '(auto-cleanup)
+    whitespace-display-mappings '((space-mark ?\u3000 [?\u25a1])
+                                   (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t]))
+    whitespace-space-regexp "\\(\\u3000+\\)"
+    whitespace-line-column 100
+    whitespace-style '(face tabs spaces trailing lines-tail space-mark tab-mark)))
