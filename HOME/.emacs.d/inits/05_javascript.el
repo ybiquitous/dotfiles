b5f92514105ecf302f935100a7e24b3abbd6d1d3
ybiquitous <ybiquitous@gmail.com>
2017-07-21 03:17:21 +0900

    automate js-auto-format-mode.el installation

--- HOME/.emacs.d/inits/05_javascript.el
+++ HOME/.emacs.d/inits/05_javascript.el
@@ -42,3 +42,10 @@
   :config
   (setq
     json-mode-indent-level tab-width))
+
+
+;; install js-auto-format-mode
+(unless (package-installed-p 'js-auto-format-mode)
+  (url-copy-file "https://raw.githubusercontent.com/ybiquitous/js-auto-format-mode/master/js-auto-format-mode.el" "js-auto-format-mode.el" t)
+  (package-install-file "js-auto-format-mode.el")
+  (delete-file "js-auto-format-mode.el"))
