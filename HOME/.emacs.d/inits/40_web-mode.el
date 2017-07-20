f4e906152699a01af4c2bb75598b93b3546b4cb2
ybiquitous <ybiquitous@gmail.com>
2017-07-20 23:29:31 +0900

    uninstall web-mode.el

--- HOME/.emacs.d/inits/40_web-mode.el
+++ HOME/.emacs.d/inits/40_web-mode.el
@@ -1,11 +1,13 @@
 (use-package web-mode
+  :ensure t
   :mode (("\\.jsp\\'" . web-mode)
-         ("\\.html\\'" . web-mode)
+          ("\\.html\\'" . web-mode)
           ("\\.erb\\'" . web-mode))
   :config
-  (setq web-mode-code-indent-offset tab-width)
-  (setq web-mode-css-indent-offset tab-width)
-  (setq web-mode-markup-indent-offset tab-width)
-  (setq web-mode-script-padding tab-width)
-  (setq web-mode-sql-indent-offset tab-width)
-  (setq web-mode-style-padding tab-width))
+  (setq
+    web-mode-code-indent-offset tab-width
+    web-mode-css-indent-offset tab-width
+    web-mode-markup-indent-offset tab-width
+    web-mode-script-padding tab-width
+    web-mode-sql-indent-offset tab-width
+    web-mode-style-padding tab-width))
