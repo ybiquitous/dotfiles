796c1e7dbce394c3898a604e6294e2b0941069c7
ybiquitous <ybiquitous@gmail.com>
2017-07-21 00:39:23 +0900

    refactor markdown-mode.el

--- HOME/.emacs.d/inits/07_markdown.el
+++ HOME/.emacs.d/inits/07_markdown.el
@@ -1,15 +1,12 @@
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-
-(add-hook 'markdown-mode-hook
-          (lambda()
-            (custom-set-variables
-             '(markdown-command "marked")
-             '(markdown-content-type "application/xhtml+xml")
-             '(markdown-css-paths
-               '("https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.8.0/github-markdown.min.css"
-                 "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css"))
-             '(markdown-xhtml-header-content "
-<script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js'></script>
-<script>document.addEventListener('DOMContentLoaded', function() { document.body.className = 'markdown-body'; hljs.initHighlightingOnLoad(); })</script>")
-             )))
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("\\.md\\'" . gfm-mode)
+          ("\\.markdown\\'" . gfm-mode))
+  :init
+  (setq
+    markdown-command "marked"
+    markdown-content-type "application/xhtml+xml"
+    markdown-css-paths '("https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.8.0/github-markdown.min.css"
+                          "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css")
+    markdown-xhtml-header-content "<script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js'></script><script>document.addEventListener('DOMContentLoaded', function() { document.body.className = 'markdown-body'; hljs.initHighlightingOnLoad(); })</script>"))
