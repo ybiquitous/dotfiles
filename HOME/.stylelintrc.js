8fa75908ed33562deb91bf0d4beea4ddbf5e0665
ybiquitous <10koba01@gmail.com>
2017-07-22 23:12:33 +0900

    feat: use `stylelint-config-standard`

--- HOME/.stylelintrc.js
+++ HOME/.stylelintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
-  "rules": {
-    "block-opening-brace-space-before": "always",
-    "color-hex-case": "lower",
-    "color-hex-length": "short",
-    "declaration-block-trailing-semicolon": "always",
-    "declaration-colon-space-after": "always-single-line",
-    "indentation": 2,
-    "string-quotes": "double"
-  }
+  extends: ['stylelint-config-standard'],
 }
