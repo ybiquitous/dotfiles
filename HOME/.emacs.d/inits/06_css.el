ffbb26e17be92ac8698068498d3354c1802ea096
ybiquitous <10koba01@gmail.com>
2017-07-22 23:43:51 +0900

    feat: official support Flycheck `stylelint` and `reek`

--- HOME/.emacs.d/inits/06_css.el
+++ HOME/.emacs.d/inits/06_css.el
@@ -1,21 +1,3 @@
-;;; stylelint (https://github.com/flycheck/flycheck/issues/785)
-(flycheck-define-checker stylelint-lint
-  "A SCSS checker using stylelint.
-
-See URL `http://stylelint.io/'."
-  :command ("stylelint" source-original)
-  :error-patterns
-  ((error line-start
-          (zero-or-more (any space)) line ":" column
-          (one-or-more (any space)) "✖" (one-or-more (any space))
-          (message)
-          (zero-or-more (any space))
-          line-end))
-  :modes (css-mode scss-mode))
-;;; stylelint
-
-(add-to-list 'flycheck-checkers 'stylelint-lint)
-
 (use-package scss-mode
   :ensure t
   :config
--- HOME/.emacs.d/inits/09_ruby.el
+++ HOME/.emacs.d/inits/09_ruby.el
@@ -42,15 +42,3 @@
     (if (file-exists-p test-file-name)
         (find-file test-file-name)
       (message "No test file: %s" test-file-name))))
-
-;; To have reek to smell-check the ruby files
-;; https://gist.github.com/franciscoj/cf308995aaabf99e33a2
-(flycheck-define-checker ruby-reek
-  "A Ruby smeel checker using reek
-See URL `https://github.com/troessner/reek'."
-  :command ("reek" "--format=xml" source-original)
-  :standard-input t
-  :error-parser flycheck-parse-checkstyle
-  :modes (enh-ruby-mode ruby-mode)
-  :next-checkers (ruby-rubocop))
-(add-to-list 'flycheck-checkers 'ruby-reek)
