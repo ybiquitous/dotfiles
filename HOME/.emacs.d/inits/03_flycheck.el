aa5f85beed7613d06eb510e5ef8047f8a2fb6d1b
ybiquitous <10koba01@gmail.com>
2017-07-28 11:55:23 +0900

    security: use `flycheck-executable-find` to avoid command injection

--- HOME/.emacs.d/inits/03_flycheck.el
+++ HOME/.emacs.d/inits/03_flycheck.el
@@ -1,5 +1,20 @@
+(defun my/find-npm-command (command)
+  (let* ((dirname "node_modules")
+          (root (locate-dominating-file default-directory dirname)))
+    (if root (concat
+               (file-name-as-directory root)
+               (file-name-as-directory dirname)
+               (file-name-as-directory ".bin")
+               command))))
+
+(defun my/executable-find (command)
+  (let* ((file-path (my/find-npm-command command)))
+    (if (and file-path (file-executable-p file-path))
+      file-path (executable-find command))))
+
 (use-package flycheck
   :init (global-flycheck-mode)
   :config
   (setq
-    flycheck-temp-prefix ".flycheck"))
+    flycheck-temp-prefix ".flycheck"
+    flycheck-executable-find #'my/executable-find))
