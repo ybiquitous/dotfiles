eab80b1007832e81a14669f03d58d5897ee30a9f
ybiquitous <10koba01@gmail.com>
2017-08-09 14:19:17 +0900

    fix shell script via shellcheck

--- install/bash-it-custom.bash
+++ install/bash-it-custom.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC1090
 . ~/.profile
 
 # https://github.com/creationix/nvm#zsh
@@ -7,12 +8,18 @@
 load-nvmrc() {
   command -v nvm >/dev/null || return
 
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
+  local node_version
+  node_version="$(nvm version)"
+
+  local nvmrc_path
+  nvmrc_path="$(nvm_find_nvmrc)"
 
   if [ -n "$nvmrc_path" ]; then
-    local nvmrc_content=$(< "${nvmrc_path}")
-    local nvmrc_node_version=$(nvm version "$nvmrc_content")
+    local nvmrc_content
+    nvmrc_content=$(< "${nvmrc_path}")
+
+    local nvmrc_node_version
+    nvmrc_node_version=$(nvm version "$nvmrc_content")
 
     if [ "$nvmrc_node_version" = "N/A" ]; then
       echo "node version '$nvmrc_content' is not installed, please run: 'nvm install'"
@@ -29,6 +36,6 @@ export PROMPT_COMMAND="$PROMPT_COMMAND ; load-nvmrc"
 # Simple workaround for Heroku completion (see https://github.com/heroku/cli/issues/224)
 _heroku_completions() {
   local cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=($(compgen -W "help $(heroku help | tail -n +4)" -- $cur))
+  COMPREPLY=($(compgen -W "help $(heroku help | tail -n +4)" -- "$cur"))
 }
 complete -F _heroku_completions heroku
