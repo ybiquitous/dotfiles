3363dc108f75ddd6dab8842462ddffaf35f78a3f
ybiquitous <10koba01@gmail.com>
2017-08-22 09:37:14 +0900

    improve markdown-mode.el configurations
    - more support GFM
    - improve readability Elisp

--- HOME/.emacs.d/inits/07_markdown.el
+++ HOME/.emacs.d/inits/07_markdown.el
@@ -6,8 +6,12 @@
   (add-hook 'markdown-mode-hook 'flyspell-mode)
   :config
   (setq
-    markdown-command "marked"
+    markdown-command "marked --tables --breaks"
     markdown-content-type "application/xhtml+xml"
     markdown-css-paths '("https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.8.0/github-markdown.min.css"
                           "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css")
-    markdown-xhtml-header-content "<script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js'></script><script>document.addEventListener('DOMContentLoaded', function() { document.body.className = 'markdown-body'; hljs.initHighlightingOnLoad(); })</script>"))
+    markdown-xhtml-header-content
+    (concat
+      "<style>.markdown-body { box-sizing: border-box; min-width: 200px; max-width: 980px; margin: 0 auto; padding: 45px; }</style>"
+      "<script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js'></script>"
+      "<script>document.addEventListener('DOMContentLoaded', function() { document.body.className = 'markdown-body'; hljs.initHighlightingOnLoad(); })</script>")))
