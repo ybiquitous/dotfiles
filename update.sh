a31cf8f4fd13d76ed6b75ac7a9ee5848788a93fb
ybiquitous <ybiquitous@gmail.com>
2017-09-07 22:27:02 +0900

    improve update.sh

--- update.sh
+++ update.sh
@@ -9,13 +9,17 @@ Please update Bash-it manually:
 EOT
 
 if [ -n "$(command -v brew)" ]; then
-  echo "brew updating..."
-  brew update && brew upgrade && brew cleanup && brew cask cleanup
   echo
+  echo "Updating brew..."
+  brew update
+  brew upgrade
+  brew cleanup
+  brew cask cleanup
 fi
 
 if [ -n "$(command -v apt-get)" ]; then
-  echo "apt updating..."
+  echo
+  echo "Updating apt..."
   sudo apt-get update
   sudo apt-get upgrade
   sudo apt-get autoremove
@@ -23,9 +27,23 @@ if [ -n "$(command -v apt-get)" ]; then
   sudo apt-get clean
 fi
 
-echo "npm updating..."
+if [ -n "$NVM_DIR" ]; then
+  echo
+  echo "Updating nvm..."
+  cd "$NVM_DIR"
+  git fetch origin
+  git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" origin)
+  . "$NVM_DIR/nvm.sh"
+fi
+
+cd "$HOME"
+
+echo
+echo "Updating npm..."
 npm -g update
 npm -g outdated || true
-echo
 
-(cd ~ ; gem update --system ; gem update --no-ri --no-rdoc)
+echo
+echo "Updating gem..."
+gem update --system
+gem update --no-ri --no-rdoc
