0f47a5b024f1e312e223d0d318253777cdc310c8
ybiquitous <10koba01@gmail.com>
2017-09-17 18:22:01 +0900

    install projectile and projectile-rails

--- HOME/.emacs.d/inits/09_ruby.el
+++ HOME/.emacs.d/inits/09_ruby.el
@@ -30,16 +30,3 @@
 (use-package ruby-electric
   :config
   (add-hook 'enh-ruby-mode-hook 'ruby-electric-mode))
-
-(defun rails-jump-to-test ()
-  "Rails Jump to Test"
-  (interactive)
-  (let* ((test-file-name (replace-regexp-in-string ".rb" "_test.rb" buffer-file-name))
-         (test-file-name (replace-regexp-in-string "/app/" "/test/" test-file-name)))
-    (unless (file-exists-p test-file-name)
-      (progn (setq test-file-name (replace-regexp-in-string "_test.rb" "_spec.rb" test-file-name)))
-      (progn (setq test-file-name (replace-regexp-in-string "/test/" "/spec/" test-file-name)))
-      )
-    (if (file-exists-p test-file-name)
-        (find-file test-file-name)
-      (message "No test file: %s" test-file-name))))
---
+++ HOME/.emacs.d/inits/50_projectile.el
@@ -0,0 +1,5 @@
+(use-package projectile)
+
+(use-package projectile-rails
+  :config
+  (projectile-rails-global-mode))
