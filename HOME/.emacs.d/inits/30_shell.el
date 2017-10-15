d0266de7418748169b14507918826d9fdacfcdf4
ybiquitous <10koba01@gmail.com>
2017-10-15 16:12:24 +0900

    refactor: settings of `shell-mode` and `eshell-mode`

--- HOME/.emacs.d/inits/30_shell.el
+++ HOME/.emacs.d/inits/30_shell.el
@@ -1,10 +1,10 @@
-(add-hook 'shell-mode-hook
-  (lambda ()
-    (local-set-key (kbd "M-p") 'comint-previous-matching-input-from-input)
-    (local-set-key (kbd "M-n") 'comint-next-matching-input-from-input)
-    (setq comint-input-ring-file-name (getenv "HISTFILE"))
-    (comint-read-input-ring)))
+(defun my/shell-mode-hook ()
+  (local-set-key (kbd "M-p") 'comint-previous-matching-input-from-input)
+  (local-set-key (kbd "M-n") 'comint-next-matching-input-from-input)
+  (setq-local comint-input-ring-file-name (getenv "HISTFILE"))
+  (comint-read-input-ring))
+(add-hook 'shell-mode-hook #'my/shell-mode-hook)
 
-(add-hook 'eshell-mode-hook
-    (lambda ()
-      (setq eshell-history-file-name (getenv "HISTFILE"))))
+(defun my/eshell-mode-hook ()
+  (setq-local eshell-history-file-name (getenv "HISTFILE")))
+(add-hook 'eshell-mode-hook #'my/eshell-mode-hook)
