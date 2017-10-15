ad12aa9505245f0de068d99eeef8e261789ec2f4
ybiquitous <10koba01@gmail.com>
2017-10-15 11:52:59 +0900

    refactor: simplify highlight-symbol-mode settings

--- HOME/.emacs.d/inits/08_highlight-symbol.el
+++ HOME/.emacs.d/inits/08_highlight-symbol.el
@@ -1,6 +1,5 @@
 (use-package highlight-symbol
-  :bind (:map highlight-symbol-mode
-          ("C-c r" . highlight-symbol-query-replace))
+  :bind ("C-c r" . highlight-symbol-query-replace)
   :config
-  (add-hook 'prog-mode-hook 'highlight-symbol-mode)
-  (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)) ;; move by M-p and M-n
+  (add-hook 'prog-mode-hook #'highlight-symbol-mode)
+  (add-hook 'prog-mode-hook #'highlight-symbol-nav-mode)) ;; move by M-p and M-n
