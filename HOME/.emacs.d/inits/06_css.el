f7a7395071a39d310a018839f60155eaec8e6b53
ybiquitous <10koba01@gmail.com>
2017-11-09 13:44:44 +0900

    feat(emacs): enable js-auto-format-mode on css-mode

--- HOME/.emacs.d/inits/06_css.el
+++ HOME/.emacs.d/inits/06_css.el
@@ -1,2 +1,8 @@
 (font-lock-add-keywords 'scss-mode
   '(("$[-[:alnum:]]+" . font-lock-constant-face)))
+
+(defun my/enable-auto-format-on-css ()
+  (setq-local js-auto-format-command "prettier")
+  (setq-local js-auto-format-command-args "--write --no-color")
+  (js-auto-format-mode))
+(add-hook 'css-mode-hook #'my/enable-auto-format-on-css)
