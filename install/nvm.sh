123981ba338c2db2489054312ec148eb83966c69
ybiquitous <ybiquitous@gmail.com>
2017-12-26 17:41:41 +0900

    feat(nvm): change from script to git for installation

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 set -eu
 
-readonly NVM_VERSION=$(curl -s https://api.github.com/repos/creationix/nvm/releases |
-                         jq -r 'map(select(.draft==false) | .name) | first')
+readonly NVM_DIR="${HOME}/.nvm"
 
-echo "Installing nvm ${NVM_VERSION}..."
-
-if [ ! -d "${HOME}/.nvm" ]; then
-  readonly DUMMY_PROFILE="${TMPDIR}/nvm_dummy_profile"
-  touch "$DUMMY_PROFILE"
-
-  # https://github.com/creationix/nvm#install-script
-  curl -s -o- "https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh" |
-    PROFILE="$DUMMY_PROFILE" bash
-
-  rm -f "$DUMMY_PROFILE"
-else
-  echo "nvm is installed already"
-fi
+# shellcheck disable=SC1090
+(
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" origin)"
+) && . "${NVM_DIR}/nvm.sh"
