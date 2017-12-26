b2d0052c59a4813f52a13661f07facb0c4b2c7d8
ybiquitous <ybiquitous@gmail.com>
2017-12-26 17:05:46 +0900

    feat(yarn): simplify installation

--- install/yarn.sh
+++ install/yarn.sh
@@ -1,23 +1,8 @@
 #!/bin/sh
 set -eu
 
-# https://yarnpkg.com/en/docs/install#alternatives-tab
-
-if [ -d ~/.yarn ]; then
-  cat <<EOT
-
-Yarn is installed already:
-
-   $(command -v yarn)
-
-If you want to reinstall, please run following command:
-
-   rm -rf ~/.yarn
-EOT
+if [ "$(uname -s)" = 'Darwin' ]; then
+  echo "Please install 'yarn' by 'brew' in Mac OS X"
 else
-  readonly DUMMY_PROFILE=/tmp/yarn-dummy-profile
-  touch $DUMMY_PROFILE
-  curl -o- -L https://yarnpkg.com/install.sh | PROFILE=$DUMMY_PROFILE bash
-  rm -rf $DUMMY_PROFILE
-  rm -rf ~/.config/yarn
+  echo "See guide: https://yarnpkg.com/en/docs/install"
 fi
