9ea332b844f22434e48b24c65b03b6ee6765055e
Masafumi Koba <ybiquitous@gmail.com>
2018-01-04 14:13:14 +0900

    fix(emacs): change function at key `C-c T`

--- HOME/.emacs.d/inits/95_google-translate.el
+++ HOME/.emacs.d/inits/95_google-translate.el
@@ -1,6 +1,6 @@
 (use-package google-translate
   :bind (("C-c t" . google-translate-at-point)
-         ("C-c T" . google-translate-query-translate))
+         ("C-c T" . google-translate-at-point-reverse))
   :init
   (setq
     google-translate-default-source-language "en"
