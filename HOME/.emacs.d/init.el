dbfbe000971f98a57d4e18d8f11ded7e4053c521
Masafumi Koba <ybiquitous@gmail.com>
2018-01-13 12:33:48 +0900

    fix(emacs): remove unnecessary MELPA stable URL

--- HOME/.emacs.d/init.el
+++ HOME/.emacs.d/init.el
@@ -77,8 +77,7 @@
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                  (not (gnutls-available-p))))
         (protocol (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa" (concat protocol "://melpa.org/packages/")) t)
-  (add-to-list 'package-archives (cons "melpa-stable" (concat protocol "://stable.melpa.org/packages/")) t))
+  (add-to-list 'package-archives (cons "melpa" (concat protocol "://melpa.org/packages/")) t))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
