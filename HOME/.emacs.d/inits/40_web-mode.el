da8eb033887b3be69483d75fefbd6d4aefe4c772
Masafumi Koba <ybiquitous@gmail.com>
2018-01-23 23:52:17 +0900

    refactor(emacs): use `:custom` instead of `setq`

--- HOME/.emacs.d/inits/40_web-mode.el
+++ HOME/.emacs.d/inits/40_web-mode.el
@@ -1,10 +1,9 @@
 (use-package web-mode
-  :mode (("\\.jsp\\'" . web-mode)
-          ("\\.html\\'" . web-mode)
-          ("\\.erb\\'" . web-mode))
-  :config
-  (setq
-    web-mode-sql-indent-offset tab-width
-    web-mode-script-padding tab-width
-    web-mode-style-padding tab-width
-    web-mode-block-padding tab-width))
+  :mode (("\\.jsp\\'"  . web-mode)
+         ("\\.html\\'" . web-mode)
+         ("\\.erb\\'"  . web-mode))
+  :custom
+  (web-mode-sql-indent-offset tab-width)
+  (web-mode-script-padding tab-width)
+  (web-mode-style-padding tab-width)
+  (web-mode-block-padding tab-width))
