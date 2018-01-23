5b7a1677507fc98d2e1cd87f0e0d74b833955b45
Masafumi Koba <ybiquitous@gmail.com>
2018-01-23 23:10:04 +0900

    fix(emacs): improve `whitespace-mode` settings
    - increase `whitespace-line-column`
    - use `:custom` instead of `setq`

--- HOME/.emacs.d/inits/20_whitespace.el
+++ HOME/.emacs.d/inits/20_whitespace.el
@@ -1,11 +1,11 @@
 (use-package whitespace
   :diminish global-whitespace-mode
+  :custom
+  (whitespace-action '(auto-cleanup))
+  (whitespace-display-mappings '((space-mark ?\u3000 [?\u25a1])
+                                 (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
+  (whitespace-space-regexp "\\(\\u3000+\\)")
+  (whitespace-line-column 120)
+  (whitespace-style '(face tabs spaces trailing lines-tail space-mark tab-mark))
   :config
-  (setq
-    whitespace-action '(auto-cleanup)
-    whitespace-display-mappings '((space-mark ?\u3000 [?\u25a1])
-                                   (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t]))
-    whitespace-space-regexp "\\(\\u3000+\\)"
-    whitespace-line-column 100
-    whitespace-style '(face tabs spaces trailing lines-tail space-mark tab-mark))
-  (global-whitespace-mode t))
+  (global-whitespace-mode))
