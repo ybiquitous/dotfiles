c410fe13e2b4c23d8c45c6ff4fb218f807fa586c
Masafumi Koba <ybiquitous@gmail.com>
2018-02-06 12:01:05 +0900

    feat(emacs): add `prettier` function

---
+++ HOME/.emacs.d/inits/96_prettier.el
@@ -0,0 +1,7 @@
+(defun prettier ()
+  (interactive)
+  (shell-command
+    (format "%s --write %s"
+      (shell-quote-argument (executable-find "prettier"))
+      (shell-quote-argument (expand-file-name buffer-file-name))))
+  (revert-buffer t t t))
