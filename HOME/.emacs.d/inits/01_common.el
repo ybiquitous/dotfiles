ef218261cab294488117f3d64d7a25d8aab28024
Masafumi Koba <ybiquitous@gmail.com>
2018-02-06 11:45:09 +0900

    fix(emacs): improve `my/keys` function

--- HOME/.emacs.d/inits/01_common.el
+++ HOME/.emacs.d/inits/01_common.el
@@ -18,10 +18,7 @@
     (_template-word-func 's-lower-camel-case))
   (defun upper-camel-case-word ()
     (interactive)
-    (_template-word-func 's-upper-camel-case))
-  (global-set-key (kbd "C-c d")   'dasherize-word)
-  (global-set-key (kbd "C-c c l") 'lower-camel-case-word)
-  (global-set-key (kbd "C-c c u") 'upper-camel-case-word))
+    (_template-word-func 's-upper-camel-case)))
 
 ;; frame title
 (setq frame-title-format '("" "%b @ Emacs " emacs-version))
--- HOME/.emacs.d/inits/99_global-keys.el
+++ HOME/.emacs.d/inits/99_global-keys.el
@@ -1,34 +1,45 @@
-(bind-key "C-h"     'delete-backward-char)
 (bind-key "C-?"     'help-command)
-(bind-key "C-c l"   'toggle-truncate-lines)
-(bind-key "C-c C-v" 'browse-url-of-buffer)
-(bind-key "C-c C-r" 'eval-buffer)
 (bind-key "C-c C-g" 'vc-git-grep)
 (bind-key "C-c C-k" 'kill-buffer-and-window)
+(bind-key "C-c C-r" 'eval-buffer)
+(bind-key "C-c C-v" 'browse-url-of-buffer)
+(bind-key "C-c c l" 'lower-camel-case-word)
+(bind-key "C-c c u" 'upper-camel-case-word)
+(bind-key "C-c d"   'dasherize-word)
+(bind-key "C-c l"   'toggle-truncate-lines)
+(bind-key "C-h"     'delete-backward-char)
 
 (defun my/keys ()
   "Display my favorite key bindings."
   (interactive)
   (display-message-or-buffer "=== My Keys
+C-? b          describe-bindings
 C-? f          describe-function
 C-? k          describe-key
-C-? b          describe-bindings
 C-M-s          isearch-forward-regexp
-M-s w          isearch-forward-word
+C-c C-c p      markdown-preview
+C-c C-g        vc-git-grep
+C-c C-k        kill-buffer-and-window
+C-c C-r        eval-buffer
+C-c c l        lower-camel-case-word
+C-c c u        upper-camel-case-word
+C-c c v        browse-url-of-buffer
+C-c d          dasherize-word
+C-c l          toggle-truncate-lines
+C-c p f        projectile-find-file
 C-q            quoted-insert (e.g. `C-q TAB')
-M-!            shell-command
-M-&            async-shell-command
 C-u M-!        shell-command
 C-u M-|        shell-command-on-region
 C-x SPC        rectangle-mark-mode
-C-x v g        vc-annotate (git blame)
 C-x v =        vc-diff (git diff)
+C-x v g        vc-annotate (git blame)
 C-x v l        vc-print-log (git log)
-C-c C-c p      markdown-preview
-C-c p f        projectile-find-file
+M-!            shell-command
+M-&            async-shell-command
+M-s w          isearch-forward-word
 
 === My Commands
 M-x align-regexp                                    Algin current region
-M-: (byte-recompile-directory \"~/.emacs.d/\" 0 t)    Recompile all *.el
 M-x yas-reload-all                                  Reload all snippets
+M-: (byte-recompile-directory \"~/.emacs.d/\" 0 t)  Recompile all *.el
 " "*My Keys*"))
