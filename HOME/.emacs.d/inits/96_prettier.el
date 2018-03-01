6795f3ffc7f9839ca3a8350fe63ab50b8c44dc76
Masafumi Koba <ybiquitous@gmail.com>
2018-03-02 04:09:36 +0900

    fix(emacs): run Flycheck after `revert-buffer` in `my/prettier`
    Otherwise Flycheck detections will be disappeared.

--- HOME/.emacs.d/inits/96_prettier.el
+++ HOME/.emacs.d/inits/96_prettier.el
@@ -4,7 +4,8 @@
     (format "%s --write %s"
       (shell-quote-argument (executable-find "prettier"))
       (shell-quote-argument (expand-file-name buffer-file-name))))
-  (revert-buffer t t t))
+  (revert-buffer t t t)
+  (if (fboundp 'flycheck-buffer) (flycheck-buffer)))
 
 (add-hook 'markdown-mode-hook
   (lambda ()
