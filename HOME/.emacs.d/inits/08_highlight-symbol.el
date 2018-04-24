c2e455b491893f521cc290e57a0abca2f4da26f4
Masafumi Koba <ybiquitous@gmail.com>
2018-04-25 08:58:51 +0900

    fix(emacs): broken highlight-symbol-mode settings

--- HOME/.emacs.d/inits/08_highlight-symbol.el
+++ HOME/.emacs.d/inits/08_highlight-symbol.el
@@ -1,6 +1,9 @@
+(defun my/enable-highlight-symbol-mode ()
+  (highlight-symbol-mode)
+  (highlight-symbol-nav-mode)) ;; move by M-p and M-n
+
 (use-package highlight-symbol
   :delight
   :bind ("C-c r" . highlight-symbol-query-replace)
   :config
-  (add-hook 'prog-mode-hook #'highlight-symbol-mode)
-  (add-hook 'prog-mode-hook #'highlight-symbol-nav-mode)) ;; move by M-p and M-n
+  (add-hook 'prog-mode-hook #'my/enable-highlight-symbol-mode))
