daa38542b0e647354c71cc30bbb0378af881203a
Masafumi Koba <ybiquitous@gmail.com>
2018-07-13 18:23:16 +0900

    fix(git): increase fill-column in git-commit-mode

--- HOME/.emacs.d/inits/90_git.el
+++ HOME/.emacs.d/inits/90_git.el
@@ -1,9 +1,13 @@
+(defun my/git-commit-mode-hook ()
+  (setq fill-column 100)
+  (flyspell-mode))
+
 (use-package git-commit
   :custom
   (git-commit-summary-max-length 100)
   :config
   (global-git-commit-mode)
-  (add-hook 'git-commit-mode-hook #'flyspell-mode))
+  (add-hook 'git-commit-mode-hook #'my/git-commit-mode-hook))
 
 ;; disable git pager
 (setenv "GIT_PAGER" "")
