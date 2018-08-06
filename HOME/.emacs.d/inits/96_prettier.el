d4db3b70128a37bcee4b2224b7101fbff80aaf3b
Masafumi Koba <ybiquitous@gmail.com>
2018-08-06 11:00:43 +0900

    fix(emacs): debug my/prettier function

--- HOME/.emacs.d/inits/96_prettier.el
+++ HOME/.emacs.d/inits/96_prettier.el
@@ -1,11 +1,12 @@
 (defun my/prettier ()
   (interactive)
-  (shell-command
-    (format "%s --write %s"
-      (shell-quote-argument (executable-find "prettier"))
-      (shell-quote-argument (expand-file-name buffer-file-name))))
-  (revert-buffer t t t)
-  (if (fboundp 'flycheck-buffer) (flycheck-buffer)))
+  (let* ((prettier-command (format "%s --write %s"
+                             (shell-quote-argument (executable-find "prettier"))
+                             (shell-quote-argument (expand-file-name buffer-file-name)))))
+    (message "Running %s..." prettier-command)
+    (shell-command prettier-command)
+    (revert-buffer t t t)
+    (if (fboundp 'flycheck-buffer) (flycheck-buffer))))
 
 (define-minor-mode my/prettier-mode
   "Minor mode for Prettier"
