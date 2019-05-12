be562853b4d0a1da64087a0bef6845bb5438229f
Masafumi Koba <ybiquitous@gmail.com>
2019-05-12 16:35:42 +0900

    fix(js): arrange npm packages
    - Remove unused packages.
    - Refactor the shell script.

--- install/npm.sh
+++ install/npm.sh
@@ -3,51 +3,17 @@ set -eu
 
 readonly NPM_INSTALL="npm install --global --no-progress"
 
-set -x
-
-# update npm
+# Update npm
 $NPM_INSTALL npm@latest
 
-# required packages
-$NPM_INSTALL \
-  alex \
-  emoj \
-  eslint \
-  fs-extra \
-  markdownlint-cli \
-  marked \
-  prettier \
-  stylelint \
-  stylelint-config-standard \
-  tern \
-  tslint \
-  tslint-config-prettier \
-  tslint-plugin-prettier \
-  tslint-react \
-  typescript \
-  typings \
-  write-good
+# Required packages
+$NPM_INSTALL eslint
+$NPM_INSTALL prettier
+$NPM_INSTALL stylelint
+$NPM_INSTALL stylelint-config-standard
+$NPM_INSTALL tern
+$NPM_INSTALL tslint
+$NPM_INSTALL typescript
 
-# optional packages
-npm_pkgs="
-msee
-sitespeed.io
-svgo"
-opt_pkgs=""
-
-for pkg in $npm_pkgs; do
-  printf "> Install '%s'? [y/N] " "$pkg"
-  read -r answer
-  case $answer in
-    [Yy]* ) opt_pkgs="$opt_pkgs $pkg" && echo "yes" ;;
-    * ) ;;
-  esac
-done
-
-if [ -n "$opt_pkgs" ]; then
-  # shellcheck disable=SC2086
-  $NPM_INSTALL $opt_pkgs
-fi
-
-# show installed packages
+# Installed packages
 npm list --global --depth=0
