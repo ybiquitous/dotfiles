d0144a324710c89e02b51d56a83e5f3ea0362b07
Masafumi Koba <ybiquitous@gmail.com>
2020-02-13 15:22:43 +0900

    feat(git): try GitHub CLI
    https://cli.github.com/

--- install/brew.sh
+++ install/brew.sh
@@ -59,6 +59,7 @@ brew install tree
 brew install watch
 brew install yarn
 brew install zsh
+brew install github/gh/gh
 brew tap heroku/brew && brew install heroku
 brew cask install docker
 brew cask install emacs
