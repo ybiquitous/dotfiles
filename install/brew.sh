1a160f0e1af14ac944b6393736d69f4b051424bc
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-22 17:28:53 +0900

    fix(brew): outdated ruby script to bash script

--- install/brew.sh
+++ install/brew.sh
@@ -18,11 +18,7 @@ EOT
 fi
 
 # https://brew.sh
-if [ -n "$(command -v brew)" ]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)" -- --force
-fi
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install aspell
 brew install awscli
