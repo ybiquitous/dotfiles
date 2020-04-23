22b3126b3feed8d22b8c2ff78434e70e7218151c
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-23 11:21:21 +0900

    feat(js): auto-install node via nvm

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-set -eu
+#!/bin/bash
+set -euo pipefail
 
 readonly NVM_DIR="${HOME}/.nvm"
 
@@ -11,3 +11,8 @@ rm -rf "$NVM_DIR"
   cd "$NVM_DIR"
   git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
 ) && \. "${NVM_DIR}/nvm.sh"
+
+nvm install --lts
+nvm alias default node
+nvm list
+node -v
