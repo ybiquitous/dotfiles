d18223ecbb2ce9c747344e082d1064f1762a69e5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-23 11:24:29 +0900

    feat(js): install yarn via npm

--- install/npm.sh
+++ install/npm.sh
@@ -13,6 +13,7 @@ $NPM_INSTALL prettier
 $NPM_INSTALL stylelint
 $NPM_INSTALL stylelint-config-recommended
 $NPM_INSTALL tern
+$NPM_INSTALL yarn
 
 # Installed packages
 npm list --global --depth=0 || true # suppress exit code 1
