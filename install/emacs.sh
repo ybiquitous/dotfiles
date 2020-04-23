68d44745afbfe6a1aa7c9f91729501ef02579b47
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-23 18:26:28 +0900

    fix(emacs): broken install script

--- install/emacs.sh
+++ install/emacs.sh
@@ -1,21 +1,17 @@
-#!/bin/sh
-set -eu
+#!/bin/bash
+set -euo pipefail
 
 # download
 readonly EMACS_DIR="${HOME}/.emacs.d"
 rm -rf "$EMACS_DIR"
-if [ "$CI" = "true" ]; then
-  git clone --depth=1 https://github.com/ybiquitous/.emacs.d.git "$EMACS_DIR"
-else
-  git clone git@github.com:ybiquitous/.emacs.d.git "$EMACS_DIR"
-fi
+git clone https://github.com/ybiquitous/.emacs.d.git "$EMACS_DIR"
 
 # customize
 readonly CONFIG_FILE="${HOME}/.emacs_env.el"
-if [ -f "$CONFIG_FILE" ]; then
+if [[ -f $CONFIG_FILE ]]; then
   echo "'${CONFIG_FILE}' already exists."
 else
-  cat <<EOF > "$CONFIG_FILE"
+  cat <<EOF > "${CONFIG_FILE}"
 ;; (add-to-list 'default-frame-alist '(top . 0))
 ;; (add-to-list 'default-frame-alist '(left . 500))
 ;; (add-to-list 'default-frame-alist '(width . 100))
