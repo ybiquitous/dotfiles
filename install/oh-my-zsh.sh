e961af1ff575869cc94242659e87a84934b7c3fb
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-23 18:59:04 +0900

    fix(zsh): install required custom theme

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 set -euo pipefail
 
-readonly OH_MY_ZSH="${HOME}/.oh-my-zsh"
-readonly OH_MY_ZSH_PLUGINS="${OH_MY_ZSH}/custom/plugins"
+dest="${HOME}/.oh-my-zsh"
 
-rm -rf "${OH_MY_ZSH}"
+rm -rf "${dest}"
 
-git clone https://github.com/robbyrussell/oh-my-zsh "${OH_MY_ZSH}"
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "${dest}"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions "${OH_MY_ZSH_PLUGINS}/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-completions "${OH_MY_ZSH_PLUGINS}/zsh-completions"
-git clone https://github.com/lukechilds/zsh-nvm "${OH_MY_ZSH_PLUGINS}/zsh-nvm"
+(
+  cd "${dest}"/custom/plugins
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git
+  git clone --depth=1 https://github.com/zsh-users/zsh-completions.git
+  git clone --depth=1 https://github.com/lukechilds/zsh-nvm.git
+)
 
-diff "${HOME}/.oh-my-zsh/templates/zshrc.zsh-template" "${HOME}/.zshrc" || true
+(
+  cd "${dest}"/custom/themes
+  git clone --depth=1 https://github.com/denysdovhan/spaceship-prompt.git
+  ln -sv spaceship-prompt/spaceship.zsh-theme spaceship.zsh-theme
+)
+
+echo
+echo '--- diff begin ---'
+diff --unified "${dest}"/templates/zshrc.zsh-template "${HOME}"/.zshrc || true
+echo '--- diff end ---'
+
+echo
+echo 'Successfully installed Oh-My-Zsh! Please restart the current session.'
