4ee2fb32bc6e5cca0e663697e2601065a908eea1
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-23 10:56:09 +0900

    feat(ruby): auto-install the latest ruby

--- install/rbenv.sh
+++ install/rbenv.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 dest="${HOME}/.rbenv"
 
+# Clean
+rm -rf "$dest"
+
 # Install
 git clone https://github.com/rbenv/rbenv.git "$dest"
 cd "$dest"
@@ -12,7 +15,17 @@ make -C src
 # Install ruby-build plugin
 git clone https://github.com/rbenv/ruby-build.git "${dest}"/plugins/ruby-build
 
-echo "To verify the installation, run the following command:"
-echo ""
-echo "    curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash"
-echo ""
+# Setup shell
+export PATH=${dest}:${PATH}
+eval "$(rbenv init -v)"
+rbenv rehash
+
+# Verify
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+
+# Install the latest Ruby
+ruby_version=$(rbenv install --list | grep -E '^2.7.' | tail -1)
+rbenv install "$ruby_version"
+rbenv global "$ruby_version"
+rbenv versions
+ruby -v
