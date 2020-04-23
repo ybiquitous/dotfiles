b999e86caf58c21704d9cd923631936df9688b9f
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-23 18:58:38 +0900

    fix(zsh): improve oh-my-zsh updating messages

--- update/oh-my-zsh.sh
+++ update/oh-my-zsh.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 
 plugins=$(find "${HOME}/.oh-my-zsh/custom/plugins" -type d -depth 1 | grep -v example)
 for plugin in $plugins; do
-  (cd "${plugin}" && echo "Updating plugin $(basename "${plugin}") ..." && git pull --no-verbose)
+  (cd "${plugin}" && echo "Updating plugin '$(basename "${plugin}")'..." && git pull --no-verbose)
 done
 
 themes=$(find "${HOME}/.oh-my-zsh/custom/themes" -type d -depth 1 | grep -v example)
 for theme in $themes; do
-  (cd "${theme}" && echo "Updating theme $(basename "${theme}") ..." && git pull --no-verbose)
+  (cd "${theme}" && echo "Updating theme '$(basename "${theme}")'..." && git pull --no-verbose)
 done
 
 echo "The update of Oh-My-ZSH completed. Please restart Zsh."
