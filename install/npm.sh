3740487fbe5b7641e6780a2067bad4f3eb9a6bc8
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-05-10 23:47:35 +0900

    feat(js): add language-server packages

--- install/npm.sh
+++ install/npm.sh
@@ -6,12 +6,14 @@ readonly NPM_INSTALL="npm install --global --no-progress"
 $NPM_INSTALL npm@latest
 
 $NPM_INSTALL eslint
+$NPM_INSTALL javascript-typescript-langserver
 $NPM_INSTALL np
 $NPM_INSTALL ntl
 $NPM_INSTALL prettier
 $NPM_INSTALL stylelint
 $NPM_INSTALL stylelint-config-recommended
 $NPM_INSTALL tern
+$NPM_INSTALL typescript-language-server
 $NPM_INSTALL yarn
 
 npm list --global --depth=0
