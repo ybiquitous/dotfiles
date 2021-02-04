742979b475d344065567e4ce8da5c8ed2ef37ba0
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-02-04 09:53:43 +0900

    refactor(js): extract `update/nvm.sh`

--- update.sh
+++ update.sh
@@ -5,15 +5,7 @@ readonly BASEDIR="${HOME}/dotfiles"
 
 "${BASEDIR}/update/brew.sh"
 "${BASEDIR}/update/apt.sh"
-
-if [ -n "$NVM_DIR" ]; then
-  # shellcheck disable=SC1090
-  (
-    cd "$NVM_DIR"
-    git fetch --tags origin
-    git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
-  ) && \. "${NVM_DIR}/nvm.sh"
-fi
+"${BASEDIR}/update/nvm.sh"
 
 npm -g update
 npm -g outdated || echo '' # ignore exit code
---
+++ update/nvm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ -n "$NVM_DIR" ]; then
+  # shellcheck disable=SC1090
+  (
+    cd "$NVM_DIR"
+    git fetch --tags origin
+    git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
+  ) && \. "${NVM_DIR}/nvm.sh"
+fi
