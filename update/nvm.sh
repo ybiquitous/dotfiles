7aff79bfa0d59ac22def69ce3f65b5b661ffaf37
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-07-16 23:39:31 +0900

    fix(js): remove --default on nvm update

--- update/nvm.sh
+++ update/nvm.sh
@@ -9,6 +9,7 @@ if [ -n "$NVM_DIR" ]; then
     git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
   ) && \. "${NVM_DIR}/nvm.sh"
 
-  nvm install --default --latest-npm
+  nvm install node --latest-npm
+  nvm alias default node
   node --version
 fi
