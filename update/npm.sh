da55d54877dc06aeb776f4bb945807533fcf08ba
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-08-31 09:51:02 +0900

    fix(js): show global npm packages on updated

--- update/npm.sh
+++ update/npm.sh
@@ -3,5 +3,9 @@ set -euo pipefail
 
 npm -g install npm
 echo "npm $(npm --version) successfully installed"
+
 npm -g update
+npm -g ls
+echo "global npm packages successfully updated"
+
 npm -g outdated || echo '' # ignore exit code
