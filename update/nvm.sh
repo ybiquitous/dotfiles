be631627d9983501fc8ca7ebe2509c79feed72fb
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-10-24 09:53:01 +0900

    fix(js): add `nvm` workaround

--- update/nvm.sh
+++ update/nvm.sh
@@ -6,7 +6,11 @@ if [ -n "$NVM_DIR" ]; then
   (
     cd "$NVM_DIR"
     git fetch --tags origin
-    git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
+    # TODO: Waiting for the next release.
+    # See https://github.com/nvm-sh/nvm/commit/fe6268c021a18068c23e96535d4a468746d3c6cf
+    git pull
+    git checkout master
+    # git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
   ) && \. "${NVM_DIR}/nvm.sh"
 
   nvm install node --latest-npm
