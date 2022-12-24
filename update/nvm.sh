e78033912c7d3deb2d66b82cafb73faf01198258
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-12-24 17:52:04 +0900

    fix(js): use the latest version 0.39.3 of nvm
    https://github.com/nvm-sh/nvm/releases/tag/v0.39.3

--- update/nvm.sh
+++ update/nvm.sh
@@ -2,16 +2,14 @@
 set -euo pipefail
 
 if [ -n "$NVM_DIR" ]; then
-  # shellcheck disable=SC1090
   (
     cd "$NVM_DIR"
     git fetch --tags origin
-    # TODO: Waiting for the next release.
-    # See https://github.com/nvm-sh/nvm/commit/fe6268c021a18068c23e96535d4a468746d3c6cf
-    git pull
-    git checkout master
-    # git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
-  ) && \. "${NVM_DIR}/nvm.sh"
+    git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
+  )
+
+  # shellcheck source=/dev/null
+  \. "${NVM_DIR}/nvm.sh"
 
   nvm install node --latest-npm
   nvm alias default node
