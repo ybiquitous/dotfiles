c86d60fe91a9673d4f5d063fc24c6a9e9a53ebb1
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-02-01 22:37:47 +0900

    feat(brew): install `dash` and `karabiner-elements`

--- install/brew.sh
+++ install/brew.sh
@@ -62,10 +62,12 @@ brew install rustup-init
 brew install shellcheck
 brew install tree
 brew install watch
+brew install --cask dash
 brew install --cask emacs
 brew install --cask google-japanese-ime
 brew install --cask imageoptim
 brew install --cask iterm2
+brew install --cask karabiner-elements
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code
 brew install --cask font-jetbrains-mono
