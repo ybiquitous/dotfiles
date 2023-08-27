3f88c9383931f7de4782cb0f257beaf95ba3e820
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-27 10:05:31 +0900

    fix(js): `reinstall-packages` when no Node.js update

--- update/nvm.sh
+++ update/nvm.sh
@@ -11,9 +11,16 @@ if [ -n "$NVM_DIR" ]; then
   # shellcheck source=/dev/null
   \. "${NVM_DIR}/nvm.sh"
 
-  nvm install node --no-progress --latest-npm --reinstall-packages-from="$(nvm current)"
+  current_version=$(nvm current)
+  nvm install node --no-progress --latest-npm
   nvm alias default node
   nvm use default
+
+  new_version=$(nvm current)
+  if [ "$current_version" -ne "$new_version" ]; then
+    nvm reinstall-packages "$new_version"
+  fi
+
   nvm ls
   echo "Newly installed Node.js $(node --version) 🎉"
 fi
