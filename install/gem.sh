ee3c758921e6733d935b7c1941e9ee066b8588af
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-07-05 17:35:55 +0900

    feat(ruby): enable `auto_install` for Bundler

--- install/gem.sh
+++ install/gem.sh
@@ -10,6 +10,7 @@ gem clean
 gem list
 
 bundle config set --global path 'vendor/bundle'
+bundle config set --global auto_install 'true'
 
 if type brew &>/dev/null; then
   bundle config set --global build.pg "--with-pg-config=${HOMEBREW_PREFIX}/opt/libpq/bin/pg_config"
