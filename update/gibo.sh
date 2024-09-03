e03fe7cbf69611138ca183f3a2a1e022ba8d6e73
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-09-03 22:33:39 +0900

    fix(git): retry when `gibo update` fails

--- update/gibo.sh
+++ update/gibo.sh
@@ -2,6 +2,13 @@
 set -euo pipefail
 
 if type gibo &>/dev/null ; then
-  gibo update
+  echo "Updating gibo..."
+
+  if ! gibo update; then
+    echo "Updating gibo failed. Removing cache and retrying update..."
+    rm -rf "$(gibo root)"
+    gibo update
+  fi
+
   "${HOME}/dotfiles/install/gibo.sh"
 fi
