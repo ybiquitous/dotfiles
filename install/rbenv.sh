8db01c661db94a3aae9f00bf2fc09af24f3639bc
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-06 10:11:08 +0900

    refactor(ruby): make rbenv installation script a bit more readable

--- install/rbenv.sh
+++ install/rbenv.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
-dest="${HOME}/.rbenv"
+rbenv_root="${HOME}/.rbenv"
+
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." &>/dev/null && pwd)"
 
 # Clean
-rm -rf "$dest"
+rm -rf "${rbenv_root}"
 
 # Install
-git clone https://github.com/rbenv/rbenv.git "$dest"
-cd "$dest"
+git clone https://github.com/rbenv/rbenv.git "${rbenv_root}"
+cd "${rbenv_root}"
 src/configure
 make -C src
 
 # Install ruby-build plugin
-git clone https://github.com/rbenv/ruby-build.git "${dest}/plugins/ruby-build"
+git clone https://github.com/rbenv/ruby-build.git "${rbenv_root}/plugins/ruby-build"
 
 # Install ruby-default-gems plugin
-git clone https://github.com/rbenv/rbenv-default-gems.git "${dest}/plugins/rbenv-default-gems"
-ln -svf "${HOME}/dotfiles/install/default-gems" "${dest}"
+git clone https://github.com/rbenv/rbenv-default-gems.git "${rbenv_root}/plugins/rbenv-default-gems"
+ln -svf "${root}/install/default-gems" "${rbenv_root}"
 
 # Setup shell
-export PATH="${dest}:${PATH}"
+export PATH="${rbenv_root}/bin:${PATH}"
 if ! type rbenv &>/dev/null ; then
   echo "rbenv is not in PATH. Try reopening the terminal."
   exit 1
@@ -32,11 +34,11 @@ rbenv rehash
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-doctor | bash
 
 # Install
-ruby_version=$(< "${HOME}/dotfiles/.ruby-version")
+ruby_version=$(< "${root}/.ruby-version")
 rbenv install "${ruby_version}" --skip-existing
 rbenv global "${ruby_version}"
 rbenv versions
 ruby -v
 
 # Set up gems
-"${HOME}/dotfiles/install/gem.sh"
+"${root}/install/gem.sh"
