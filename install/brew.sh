f41cfc3ff5228090d016e2e5aa4f222efc903b2a
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-08-16 13:36:12 +0900

    feat(shell): install Nerd Fonts and configure it for `lsd` icons
    https://www.nerdfonts.com/

--- install/brew.sh
+++ install/brew.sh
@@ -80,6 +80,7 @@ brew install --cask keycastr
 brew install --cask musescore
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code
+brew install --cask font-fira-code-nerd-font
 brew install --cask font-jetbrains-mono
 brew cleanup
 brew doctor
