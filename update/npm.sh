453bbbe5613dc9e52638b674a7175c54ff20e276
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-08-30 09:29:39 +0900

    feat(js): activate nvm if it exists before updating npm packages

--- update/npm.sh
+++ update/npm.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 set -euo pipefail
 
+# Load nvm if it exists.
+if [[ -n "${NVM_DIR:-}" ]]; then
+  # shellcheck source=/dev/null
+  . "${NVM_DIR}/nvm.sh"
+  echo "Using nvm v$(nvm -v)"
+  nvm use default
+fi
+
+echo "Current Node.js version: $(node --version)"
+echo
+
+echo "🔄 Installing the latest npm version..."
 npm -g install npm
 echo
 echo "✅ Successfully installed npm \"$(npm --version)\" "
+echo
 
+echo "🔄 Updating global npm packages..."
 npm -g update
 npm -g ls
 echo
 echo '✅ Successfully updated global npm packages'
+echo
 
 npm -g outdated || echo '' # ignore exit code
 
 (
   cd "$(dirname "${BASH_SOURCE[0]}")/.."
-  npm ci
+  echo "🔄 Fresh-installing local npm packages..."
+  npm ci --loglevel=error
   echo
-  echo "✅ Successfully updated local npm packages in ${PWD}"
+  echo "✅ Successfully fresh-installed local npm packages in ${PWD}"
 )
