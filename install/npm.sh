bc9c14078e65156adecb354a66b112612b9e5f3d
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-01-31 12:54:06 +0900

    feat(js): install some npm packages globally

--- install/npm.sh
+++ install/npm.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "Installing/updating npm packages globally..."
 readonly install="npm install --global --no-progress"
+$install \
+  npm@latest \
+  @actions/languageserver@latest \
+  @github/copilot-language-server@latest \
+  typescript-language-server@latest \
+  vscode-json-languageserver@latest
 
-$install npm@latest
-
+echo
+echo "Showing installed npm packages..."
+echo
 npm list --global --depth=0
