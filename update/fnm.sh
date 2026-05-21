42bd5902653aeaa90ac5e81ada98e8f4bf43acf5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-05-21 20:34:22 +0900

    fix(js): set latest version as default after upgrade via fnm

--- update/fnm.sh
+++ update/fnm.sh
@@ -5,15 +5,15 @@ if type fnm &>/dev/null; then
   current_version=$(fnm current)
 
   fnm install --latest
-  fnm default "$(fnm current)"
+  fnm default latest
   fnm use default
 
-  new_version=$(fnm current)
-  if [ "$current_version" != "$new_version" ]; then
-    echo "Updated from $current_version to $new_version"
+  new_version="$(fnm current)"
+  if [[ "${current_version}" != "${new_version}" ]]; then
+    echo "✅Updated from ${current_version} to ${new_version}"
   fi
 
   fnm list
   echo ''
-  printf 'Node.js \x1b[1m%s\x1b[0m\n' "$(node --version)"
+  printf 'The default Node.js version: \x1b[1m%s\x1b[0m\n' "$(node --version)"
 fi
