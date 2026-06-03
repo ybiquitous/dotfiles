306ec25979c635c723ea2dfcfb0310149888ec3e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-06-04 00:06:07 +0900

    feat(ruby): set up Bundler on install/update events with new cooldown/checksums settings

--- install/gem.sh
+++ install/gem.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+echo '> Updating gems...'
 gem update --system
 gem update
 gem clean
 gem list
 
-bundle config set --global path 'vendor/bundle'
-bundle config set --global auto_install 'true'
-
-if type brew &>/dev/null; then
-  bundle config set --global build.pg "--with-pg-config=${HOMEBREW_PREFIX}/opt/libpq/bin/pg_config"
-fi
-
-bundle config
+echo ''
+echo '> Setting up Bundler...'
+"${HOME}/bin/setup_bundler"
--- update/gem.sh
+++ update/gem.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+echo '> Updating gems...'
 gem update --system
 gem update
 gem cleanup
 gem outdated
+
+echo ''
+echo '> Setting up Bundler...'
+"${HOME}/bin/setup_bundler"
