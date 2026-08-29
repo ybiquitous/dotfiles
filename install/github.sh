a6a4f68d1074af9a64ebc19d3197d66baf3502f5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-08-29 21:20:17 +0900

    feat(github)!: remove `gh poi` extension
    Run `gh extension remove poi` to uninstall it.
    
    The `git sync` alias can now do the same.

--- install/github.sh
+++ install/github.sh
@@ -3,10 +3,8 @@ set -euo pipefail
 
 if type gh &>/dev/null; then
   # Extensions
-  extensions=(
-    seachicken/gh-poi
-  )
-  for extension in "${extensions[@]}"; do
+  extensions=()
+  for extension in "${extensions[@]+"${extensions[@]}"}"; do
     gh extension install "${extension}"
   done
   gh extension list
